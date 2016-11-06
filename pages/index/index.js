f42dc390afc74ea653f877774edb92c77644a04d
ahonn <ahonn95@outlook.com>
2016-11-06 23:10:01 +0800

    update index page vols data

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,22 +5,45 @@ Page({
   },
   onLoad: function () {
     var that = this
-    var now = new Date()
-    var strDate = now.getFullYear() + '-' + (now.getMonth() + 1)
-    
     wx.request({
-      url: 'http://v3.wufazhuce.com:8000/api/hp/bymonth/' + strDate, 
+      url: 'http://v3.wufazhuce.com:8000/api/hp/idlist/0', 
       header: {
         'Content-Type': 'application/json'
       },
       success: function(res) {
         if (res.data.res === 0) {
-          var data = res.data.data
-          data.map(function (item) {
-            item.hp_makettime = util.formatMakettime(item.hp_makettime)
+          var idList = res.data.data.slice(0, 9)
+          // data.map(function (item) {
+          //   item.hp_makettime = util.formatMakettime(item.hp_makettime)
+          // })
+          idList.map(function (id) {
+            that.getVols(id)
           })
+        }
+      }
+    })
+  },
+  getVols: function (id) {
+    var that = this
+    wx.request({
+      url: 'http://v3.wufazhuce.com:8000/api/hp/detail/' + id,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        if (res.data.res === 0) {
+          var vol = res.data.data
+          var volsData = that.data.volsData
+          
+          vol.date = new Date(vol.hp_makettime)
+          vol.hp_makettime = util.formatMakettime(vol.hp_makettime)
+          volsData.push(vol)
+          volsData.sort(function (a, b) {
+            return b.date - a.date
+          })
+
           that.setData({
-            volsData: data
+            volsData: volsData
           })
         }
       }
